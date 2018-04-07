4af388f5f205b5eb4ded3891795bde89cb78b62c
Vered Rekanati <veredrec@gmail.com>
2018-04-07 07:49:07 -0400

    Convert js to jquery

--- script.js
+++ script.js
@@ -22,9 +22,8 @@ function populateInput() {
 var storedData = [];
 var retrievedData = [];
 
-var submitForm = document.getElementById('submit');
-
-submitForm.addEventListener('click', function(e) {
+// submit habits form
+$('#submit').on('click', function(e) {
   e.preventDefault();
 
   var task1Val = task1.value;
@@ -45,7 +44,6 @@ submitForm.addEventListener('click', function(e) {
   if (!tasks.every(checkInput)) {
     $('#message').removeClass('hide-message');
   } else {
-    console.log('returning class');
     $('#message').addClass('hide-message');
     storeData(tasks);
     moveToTrackPage();
@@ -76,22 +74,14 @@ function getTasks() {
 
 // Populate track part with the data
 function populateTrack(tasksArray) {
-  var track1 = document.querySelector('.track1');
-  var track2 = document.querySelector('.track2');
-  var track3 = document.querySelector('.track3');
-  var track4 = document.querySelector('.track4');
-  var track5 = document.querySelector('.track5');
-
-  track1.textContent = tasksArray[0].task;
-  track2.textContent = tasksArray[1].task;
-  track3.textContent = tasksArray[2].task;
-  track4.textContent = tasksArray[3].task;
-  track5.textContent = tasksArray[4].task;
+  $('.track1').text(tasksArray[0].task);
+  $('.track2').text(tasksArray[1].task);
+  $('.track3').text(tasksArray[2].task);
+  $('.track4').text(tasksArray[3].task);
+  $('.track5').text(tasksArray[4].task);
 }
 
 // Check off the completed tasks
-
-// var completed = document.getElementsByClassName('completed');
 $('.item-icon').on('click', function(e) {
   $(this)
     .find('.checkmark')
@@ -100,57 +90,39 @@ $('.item-icon').on('click', function(e) {
 
 // -------------------------------------------------
 // Navigate between the parts ("pages")
-var form = document.getElementById('form');
-var track = document.getElementById('track');
-var stats = document.getElementById('stats');
-var settings = document.getElementById('settings');
 
-var formBtn = document.getElementById('formBtn');
-var trackBtn = document.getElementById('trackBtn');
-var statsBtn = document.getElementById('statsBtn');
-var settingsBtn = document.getElementById('settingsBtn');
-
-formBtn.addEventListener('click', function(e) {
+$('#formBtn').on('click', function(e) {
   e.preventDefault();
-  form.classList.remove('hide-part');
-  track.classList.add('hide-part');
-  stats.classList.add('hide-part');
-  settings.classList.add('hide-part');
+  $('#form').removeClass('hide-part');
+  $('#track').addClass('hide-part');
+  $('#stats').addClass('hide-part');
+  $('#settings').addClass('hide-part');
 });
 
-trackBtn.addEventListener('click', function(e) {
+$('#trackBtn').on('click', function(e) {
   e.preventDefault();
   moveToTrackPage();
 });
 
-statsBtn.addEventListener('click', function(e) {
+$('#statsBtn').on('click', function(e) {
   e.preventDefault();
-  stats.classList.remove('hide-part');
-  form.classList.add('hide-part');
-  track.classList.add('hide-part');
-  settings.classList.add('hide-part');
+  $('#stats').removeClass('hide-part');
+  $('#form').addClass('hide-part');
+  $('#track').addClass('hide-part');
+  $('#settings').addClass('hide-part');
 });
 
-settingsBtn.addEventListener('click', function(e) {
+$('#settingsBtn').on('click', function(e) {
   e.preventDefault();
-  settings.classList.remove('hide-part');
-  form.classList.add('hide-part');
-  track.classList.add('hide-part');
-  stats.classList.add('hide-part');
+  $('#settings').removeClass('hide-part');
+  $('#form').addClass('hide-part');
+  $('#track').addClass('hide-part');
+  $('#stats').addClass('hide-part');
 });
+
 function moveToTrackPage() {
-  track.classList.remove('hide-part');
-  form.classList.add('hide-part');
-  stats.classList.add('hide-part');
-  settings.classList.add('hide-part');
+  $('#track').removeClass('hide-part');
+  $('#form').addClass('hide-part');
+  $('#stats').addClass('hide-part');
+  $('#settings').addClass('hide-part');
 }
-// Maybe the above code can be refactored similar to this:
-
-// var pages = document.querySelectorAll('.page');
-// pages.forEach(function(page) {
-//   page.addEventListener('click', function(e) {
-//     e.preventDefault();
-//     console.log('PAGE CLICKED ', this);
-//     page.classList.toggle('hide-part');
-//   });
-// });
